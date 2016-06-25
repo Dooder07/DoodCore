d33a789ea9a2ebdf39b66cdf93cad7add449a996
Dooder07 <cmoshields7@gmail.com>
2016-06-25 01:23:39 -0400

    Solution for FancyMessage

--- src/main/java/net/doodcraft/dooder07/spigot/doodcore/Methods.java
+++ src/main/java/net/doodcraft/dooder07/spigot/doodcore/Methods.java
@@ -2,6 +2,9 @@ package net.doodcraft.dooder07.spigot.doodcore;
 
 import mkremins.fanciful.FancyMessage;
 import net.doodcraft.dooder07.spigot.doodcore.config.Settings;
+import net.minecraft.server.v1_10_R1.IChatBaseComponent;
+import net.minecraft.server.v1_10_R1.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 /**
@@ -30,6 +33,14 @@ import org.bukkit.entity.Player;
 public class Methods {
     public static void sendNoPermission(Player player, String node) {
         FancyMessage message = new FancyMessage(StringParser.addColor(Settings.pluginPrefix + " &cNo permission")).tooltip(StringParser.addColor("&b" + node));
-        message.send(player);
+        sendFancyMessage(player, message);
+    }
+
+    public static PacketPlayOutChat createPacketPlayOutChat(String s){return new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a(s));}
+
+    public static void sendJsonMessage(Player p, String s){( (CraftPlayer)p ).getHandle().playerConnection.sendPacket(createPacketPlayOutChat(s));}
+
+    public static void sendFancyMessage(Player player, FancyMessage message) {
+        sendJsonMessage(player, message.toJSONString());
     }
 }
